bf739e45b368fbc42d549dfc4dd292a554dd2fab
kennyrukoro <k.rukoro@alustudent.com>
2025-09-12 08:08:42 -0700

    created test and models file

---
+++ models/base_models.py
@@ -0,0 +1,24 @@
+"""this model will contain public attributes and methods"""
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        my_dict = self.__dict__.copy()
+        my_dict['__class__'] = self.__class__.__name__
+        my_dict['created_at'] = self.created_at.isoformat()
+        my_dict['updated_at'] = self.updated_at.isoformat()
+        return my_dict
+
+
