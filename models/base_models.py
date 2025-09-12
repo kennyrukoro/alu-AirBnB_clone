ca24088bd5ec7e11f44e916e9e4d06a238cea853
Frankish0014 <f.ishimwe@alustudent.com>
2025-09-12 17:49:59 +0200

    updated the base file

--- models/base_models.py
+++ models/base_models.py
@@ -1,24 +1,45 @@
 """this model will contain public attributes and methods"""
 import uuid
 from datetime import datetime
+from models import storage
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    """Class frmo whci all the classes will inherit"""
+    def __init__(self, *args, **kwargs):
+        """Initializes instance attributes
+        args: 
+        *args: list of arguments # Name["frank", "john", "doe"]
+        ** kwargs: dictionary of key/values arguments # {"name": "frank", "age": 23}
+        """
+        
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(kwargs["created_at"], "%Y/%m/%dT %H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(kwargs["updated_at"], "%Y/%m/%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-
+        return "({}) ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
+    
+    
     def save(self):
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         my_dict = self.__dict__.copy()
-        my_dict['__class__'] = self.__class__.__name__
-        my_dict['created_at'] = self.created_at.isoformat()
-        my_dict['updated_at'] = self.updated_at.isoformat()
+        my_dict['__class__'] = type(self).__name__
+        my_dict['created_at'] = my_dict["created_at"].isoformat()
+        my_dict['updated_at'] = my_dict["updated_at"].isoformat()
         return my_dict
 
 
